a8c64f827da921788b3d886d27721824b6f55e50
Max <maxy.finke@icloud.com>
2020-02-06 11:21:22 -0800

    Player movement and boundaries

---
+++ GeoFlyer/Assets/Boundaries.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.8f, 1.8f),
+            Mathf.Clamp(transform.position.y, -2f, -2f), transform.position.z);
+
+    }
+}
