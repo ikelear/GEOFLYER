ac4b1f29d69238d966007348b0fef0f02037d4eb
Ike <ike.lear@gmail.com>
2020-02-10 11:23:17 -0800

    LeaderBoard Start

---
+++ GeoFlyer/Assets/IkesScripts/StartMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void GotoLeaderBoard()
+    {
+        SceneManager.LoadScene(sceneName: "LeaderBoard");
+    }
+
+    public void GotoStartMenu()
+    {
+        SceneManager.LoadScene(sceneName: "StartMenu");
+    }
+}
