d069c811c60cd8062630989882eabea28c1c3d4e
Max <maxy.finke@icloud.com>
2020-02-12 11:22:04 -0800

    Crash Functional

---
+++ GeoFlyer/Assets/Scripts/deployBlimp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployBlimp : MonoBehaviour
+{
+    public GameObject blimpPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(blimpWave());
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(blimpPrefab) as GameObject;
+        a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x) , screenBounds.y * 2);
+    }
+
+    IEnumerator blimpWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+
+   
+}
---
+++ GeoFlyer/Assets/Scripts/plane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class plane : MonoBehaviour
+{
+    public GameObject explosion;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("hit detected");
+        GameObject e = Instantiate(explosion) as GameObject;
+        e.transform.position = transform.position;
+        Destroy(other.gameObject);
+        transform.position = new Vector3(0, -20, 0);
+
+        StartCoroutine(ExecuteAfterTime(3));
+        
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        Debug.Log(time);
+        yield return new WaitForSeconds(time);
+        Debug.Log("Success!");
+        this.gameObject.SetActive(false);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
