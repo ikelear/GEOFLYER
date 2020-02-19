41b56dc217726cd3e0cc3fe163f778b4acc3f668
Max <maxy.finke@icloud.com>
2020-02-19 11:15:54 -0800

    Added Coins

--- GeoFlyer/Assets/Scripts/plane.cs
+++ GeoFlyer/Assets/Scripts/plane.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class plane : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject explosiondetector;
+    public GameObject coin;
+    public Text txt;
+    float money;
+
+    private void Start()
+    {
+        money = 0f;
+        txt.text = money.ToString();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-        GameObject e = Instantiate(explosion) as GameObject;
-        e.transform.position = transform.position;
-        Destroy(other.gameObject);
-        transform.position = new Vector3(0, -20, 0);
-        explosiondetector.SetActive(false);
+        if(other.transform.tag == "enemy")
+        {
+            GameObject e = Instantiate(explosion) as GameObject;
+            e.transform.position = transform.position;
+            Destroy(other.gameObject);
+            transform.position = new Vector3(0, -20, 0);
+            explosiondetector.SetActive(false);
 
-        StartCoroutine(ExecuteAfterTime(3));
+            StartCoroutine(ExecuteAfterTime(3));
+        }
+        if(other.transform.tag == "coin")
+        {
+            Destroy(other.gameObject);
+            money = money + 1.0f;
+            txt.text = money.ToString();
+        }
         
+
+
     }
     IEnumerator ExecuteAfterTime(float time)
     {
